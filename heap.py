147fa64ed5d9e0995eacc0ea52211ed0cde27a47
gabrielamendezg <44039936+gabrielamendezg@users.noreply.github.com>
2018-12-10 00:36:02 -0300

    Add files via upload

--- heap.py
+++ heap.py
@@ -1,30 +1,4 @@
 import os
-
-def upheap(datos,pos,cmp):
-	if pos==0: return
-	pos_padre = (pos-1)//2
-	if cmp(datos[pos],datos[pos_padre]) > 0: return
-	datos[pos],datos[pos_padre] = datos[pos_padre],datos[pos]
-	upheap(datos,pos_padre,cmp)
-
-def downheap(datos,n,pos,cmp):
-	if pos >= n : return
-	pos_h_izq = (2*pos)+1
-	pos_h_der = (2*pos)+1
-	pos_min = pos
-	if pos_h_izq < n and cmp(datos[pos_h_izq],datos[pos_min]) < 0 :
-		pos_min = pos_h_izq
-	if pos_h_der < n and cmp(datos[pos_h_der],datos[pos_min]) < 0 :
-		pos_min = pos_h_der
-	if pos != pos_min:
-		datos[pos],datos[pos_min]=datos[pos_min],datos[pos]
-		downheap(datos,n,pos_min,cmp)
-	return
-
-def heapify(arr,n,cmp):
-	for x in range(n,0,-1):
-		downheap(datos,x-1,cmp)
-
 class Heap:
 	def __init__(self,f_cmp):
 		self.cant = 0
@@ -40,10 +14,35 @@ class Heap:
 	def esta_vacio(self):
 		return self.cant == 0
 
+	def _upheap(self,datos,pos,cmp):
+		if pos==0: return
+		pos_padre = (pos-1)//2
+		if cmp(datos[pos],datos[pos_padre]) > 0: return
+		datos[pos],datos[pos_padre] = datos[pos_padre],datos[pos]
+		self._upheap(datos,pos_padre,cmp)
+
+	def _downheap(self,datos,n,pos,cmp):
+		if pos >= n : return
+		pos_h_izq = (2*pos)+1
+		pos_h_der = (2*pos)+1
+		pos_min = pos
+		if pos_h_izq < n and cmp(datos[pos_h_izq],datos[pos_min]) < 0 :
+			pos_min = pos_h_izq
+		if pos_h_der < n and cmp(datos[pos_h_der],datos[pos_min]) < 0 :
+			pos_min = pos_h_der
+		if pos != pos_min:
+			datos[pos],datos[pos_min]=datos[pos_min],datos[pos]
+			self._downheap(datos,n,pos_min,cmp)
+		return
+
+	def heapify(self,arr,n,cmp):
+		for x in range(n,0,-1):
+			self._downheap(datos,x-1,cmp)
+
 	def encolar(self,dato):
 		if dato is None: return False
 		self.datos.append(dato)
-		upheap(self.datos,self.cant,self.cmp)
+		self._upheap(self.datos,self.cant,self.cmp)
 		self.cant+=1
 		return True
 
@@ -55,5 +54,5 @@ class Heap:
 		if self.esta_vacio(): return None
 		elem = self.datos.pop(0)
 		self.cant-=1
-		downheap(self.datos,self.cant,0,self.cmp)
+		self._downheap(self.datos,self.cant,0,self.cmp)
 		return elem
